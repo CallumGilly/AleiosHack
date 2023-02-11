e52680dffbff90cef5b7bc09ad3463768162d3e5
CallumGilly <cg3g22@soton.ac.uk>
2023-02-11 22:42:32 +0000

    Column names

--- dtan.py
+++ dtan.py
@@ -5,11 +5,12 @@ from geopy.distance import geodesic
 
 db_connection = sql.connect(host='localhost',database='Hack',user='pythonApp',password='VerySecure127!')
 db_cursor = db_connection.cursor()
-db_cursor.execute('SELECT * FROM ')
+db_cursor.execute('SELECT * FROM Hack.reports')
 table_rows = db_cursor.fetchall()
 df = pd.DataFrame(table_rows)
 
 rows_count = len(df.index)
+print(df)
 df["Group"] = ""
 
 group = 1
@@ -18,11 +19,11 @@ for i in range(0,rows_count):
     if df["Group"][i]=="":
         df["Group"][i] = group
 
-        location = (df["latitude"].values[i],df["longitude"].values[i])
+        location = (df[2].values[i],df[1].values[i])
     
         for x in range(i+1,rows_count):
-            loccomp = (df["latitude"].values[x],df["longitude"].values[x])
-            if(df["prob"].values[i]==df["prob"].values[x] and geodesic(location,loccomp) <=0.02):
+            loccomp = (df[2].values[x],df[1].values[x])
+            if(df[5].values[i]==df[5].values[x] and geodesic(location,loccomp) <=0.02):
                 df["Group"][x]=group
             
         group=group+1
