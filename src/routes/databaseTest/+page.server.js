39193bcf1ce25ffd69c885379048155c7c91f049
Callum Gilchrist <43007634+CallumGilly@users.noreply.github.com>
2023-02-11 23:48:56 +0000

    A stunner

--- src/routes/databaseTest/+page.server.js
+++ src/routes/databaseTest/+page.server.js
@@ -1,6 +1,6 @@
 // import { createRequire } from "module";
 // const require = createRequire(import.meta.url);
-import getRowsFromDB from "../../lib/server/database";
+import getRowsFromDB from "$lib/server/database";
 // const mysql = require("mysql");
 // export function async load() {
 export const load = async ({ cookies }) => {
