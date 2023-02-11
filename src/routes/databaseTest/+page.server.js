e8ee5765f449eb575cabcbd1f85c08b59b5b6bbb
Callum Gilchrist <43007634+CallumGilly@users.noreply.github.com>
2023-02-11 16:34:05 +0000

    Whats life if not a commment removing code

--- src/routes/databaseTest/+page.server.js
+++ src/routes/databaseTest/+page.server.js
@@ -27,45 +27,45 @@ export const load = ({ cookies }) => {
 };
 
 export const actions = {
-	/**
-	 * Modify game state in reaction to a keypress. If client-side JavaScript
-	 * is available, this will happen in the browser instead of here
-	 */
-	update: async ({ request, cookies }) => {
-		const game = new Game(cookies.get('sverdle'));
+	// /**
+	//  * Modify game state in reaction to a keypress. If client-side JavaScript
+	//  * is available, this will happen in the browser instead of here
+	//  */
+	// update: async ({ request, cookies }) => {
+	// 	const game = new Game(cookies.get('sverdle'));
 
-		const data = await request.formData();
-		const key = data.get('key');
+	// 	const data = await request.formData();
+	// 	const key = data.get('key');
 
-		const i = game.answers.length;
+	// 	const i = game.answers.length;
 
-		if (key === 'backspace') {
-			game.guesses[i] = game.guesses[i].slice(0, -1);
-		} else {
-			game.guesses[i] += key;
-		}
+	// 	if (key === 'backspace') {
+	// 		game.guesses[i] = game.guesses[i].slice(0, -1);
+	// 	} else {
+	// 		game.guesses[i] += key;
+	// 	}
 
-		cookies.set('sverdle', game.toString());
-	},
+	// 	cookies.set('sverdle', game.toString());
+	// },
 
-	/**
-	 * Modify game state in reaction to a guessed word. This logic always runs on
-	 * the server, so that people can't cheat by peeking at the JavaScript
-	 */
-	enter: async ({ request, cookies }) => {
-		const game = new Game(cookies.get('sverdle'));
+	// /**
+	//  * Modify game state in reaction to a guessed word. This logic always runs on
+	//  * the server, so that people can't cheat by peeking at the JavaScript
+	//  */
+	// enter: async ({ request, cookies }) => {
+	// 	const game = new Game(cookies.get('sverdle'));
 
-		const data = await request.formData();
-		const guess = (data.getAll('guess'));
+	// 	const data = await request.formData();
+	// 	const guess = (data.getAll('guess'));
 
-		if (!game.enter(guess)) {
-			return fail(400, { badGuess: true });
-		}
+	// 	if (!game.enter(guess)) {
+	// 		return fail(400, { badGuess: true });
+	// 	}
 
-		cookies.set('sverdle', game.toString());
-	},
+	// 	cookies.set('sverdle', game.toString());
+	// },
 
-	restart: async ({ cookies }) => {
-		cookies.delete('sverdle');
-	}
+	// restart: async ({ cookies }) => {
+	// 	cookies.delete('sverdle');
+	// }
 };
