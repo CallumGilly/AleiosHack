2adc6818e10ee00fa645b94d2077cef04035de66
Bob Hart <77585854+bhart17@users.noreply.github.com>
2023-02-12 12:28:48 +0000

    List events

--- src/routes/analytics/+page.svelte
+++ src/routes/analytics/+page.svelte
@@ -14,12 +14,7 @@
 
 <div class="app">
 	<section id="analytics-page">
-		{#each data.arr as event}
-			<div class="event-item">
-				<p>Category: {event.Category}<br>Description: {event.description}<br>Coordinates: {event.Latitude}, {event.Longitude}<br>Time: {event.Time}</p>
-			</div>
-		{/each}
-		<!-- <table
+		<table
 			id="chart"
 			class="charts-css column show-heading show-labels show-primary-axis show-4-secondary-axes show-data-axes data-spacing-15"
 		>
@@ -34,27 +29,32 @@
 
 			<tbody>
 				<tr>
-					<th> 2016 </th>
-					<td style="--size:0.2;" />
+					<th> October </th>
+					<td style="--size:0.3;" />
 				</tr>
 				<tr>
-					<th> 2017 </th>
+					<th> November </th>
 					<td style="--size:0.4;" />
 				</tr>
 				<tr>
-					<th> 2018 </th>
-					<td style="--size:0.6;" />
+					<th> December </th>
+					<td style="--size:0.2;" />
 				</tr>
 				<tr>
-					<th> 2019 </th>
-					<td style="--size:0.8;" />
+					<th> January </th>
+					<td style="--size:0.7;" />
 				</tr>
 				<tr>
-					<th> 2020 </th>
+					<th> February </th>
 					<td style="--size:1.0;" />
 				</tr>
 			</tbody>
-		</table> -->
+		</table>
+		{#each data.arr as event}
+			<div class="event-item">
+				<p>Category: {event.Category}<br>Description: {event.description}<br>Coordinates: {event.Latitude}, {event.Longitude}<br>Time: {event.Time}</p>
+			</div>
+		{/each}
 	</section>
 
 	<CloseButton />
@@ -67,6 +67,7 @@
 	}
 
 	.app {
+		overflow-y: auto;
 		display: flex;
 		flex-direction: column;
 		min-height: 100vh;
