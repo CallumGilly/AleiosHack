75d79b7027d503b242a31212b51476748514e26a
Bob Hart <77585854+bhart17@users.noreply.github.com>
2023-02-12 10:23:01 +0000

    package.json

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,29 +1,30 @@
 import query from "$lib/server/database";
 
 export async function load() {
-    let dbResponse = await query('SELECT * FROM Hack.reports', []);
+    // let dbResponse = await query('SELECT * FROM Hack.reports', []);
 
-    return { arr: JSON.parse(JSON.stringify(dbResponse)) };
-    // return {
-    //     arr: [
-    //         {
-    //             reportID: 5,
-    //             Longitude: -1.401555,
-    //             Latitude: 50.954955,
-    //             Image: null,
-    //             Time: '2023-02-12T06:40:15.000Z',
-    //             Category: 'Litter',
-    //             Description: 'There rubbish here'
-    //         },
-    //         {
-    //             reportID: 6,
-    //             Longitude: -1.401555,
-    //             Latitude: 50.954955,
-    //             Image: null,
-    //             Time: '2023-02-12T06:42:10.000Z',
-    //             Category: 'Water Wastage',
-    //             Description: 'The taps on'
-    //         }
-    //     ]
-    // }
+    // return { arr: JSON.parse(JSON.stringify(dbResponse)) };
+
+    return {
+        arr: [
+            {
+                reportID: 5,
+                Longitude: -1.401555,
+                Latitude: 50.954955,
+                Image: null,
+                Time: '2023-02-12T06:40:15.000Z',
+                Category: 'Litter',
+                Description: 'There rubbish here'
+            },
+            {
+                reportID: 6,
+                Longitude: -1.401555,
+                Latitude: 50.954955,
+                Image: null,
+                Time: '2023-02-12T06:42:10.000Z',
+                Category: 'Water Wastage',
+                Description: 'The taps on'
+            }
+        ]
+    }
 }
