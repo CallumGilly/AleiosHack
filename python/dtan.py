98eea074d89ce5460550fee289f877b52e9e59ca
Naomi Pitzer <91554831+naypitzer@users.noreply.github.com>
2023-02-12 07:04:54 +0000

    oa

--- python/dtan.py
+++ python/dtan.py
@@ -31,6 +31,7 @@ for i in range(0,rows_count):
         group=group+1
 
 df[8]=1
+print(df)
 
 groupeddf = df.groupby(7,as_index=False).agg({1:'mean',2:'mean',8:'sum',5:'min'})
 groupeddf = groupeddf.sort_values(by=[8],ascending=False)
@@ -42,21 +43,6 @@ for i in range(0,len(groupeddf.index)):
     groupeddf[9][i]=i
     i=i+1
 print(groupeddf)
-
-groupeddf[10] = ""
-
-for i in range(1,max(groupeddf[7])):
-    df1 = df[df[7]==i]
-    df1 = df1.drop(df1.iloc[:,[0,1,2,3,5,6]],axis=1)
-    df1 = df1.sort_values(by=[4],inplace=True)
-    x = df1[4]
-    df1[4] = df1[4].map(df.datetime.toordinal)
-
-    f = np.polyfit(df1[4], df1[8], deg=1)
-    fin = f[0]
-    for x in range(1,len(groupeddf.index)):
-        if(df[7][x]==i):
-            groupeddf[10][x]= fin
 
 print(groupeddf)
 groupeddf.to_csv(r'/home/cwjg21/AleiosHack/python/analytics.txt',header=True,index=None,sep=',')
