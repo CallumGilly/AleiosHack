aec9cca65fc8b9e04d000a8681e506c4e3179475
Naomi Pitzer <91554831+naypitzer@users.noreply.github.com>
2023-02-12 06:55:37 +0000

    tst2

--- python/dtan.py
+++ python/dtan.py
@@ -35,8 +35,12 @@ df[8]=1
 groupeddf = df.groupby(7,as_index=False).agg({1:'mean',2:'mean',8:'sum',5:'min'})
 groupeddf = groupeddf.sort_values(by=[8],ascending=False)
 print(groupeddf)
-y=[range(1,len(groupeddf.index))]
-groupeddf[9]=y
+
+groupeddf[9]=""
+y=1
+for i in range(0,len(groupeddf.index)):
+    groupeddf[9][i]=i
+    i=i+1
 
 groupeddf[10] = ""
 
