626a7d8a41d6f88ddd609eb766b183f4285779a9
Naomi Pitzer <91554831+naypitzer@users.noreply.github.com>
2023-02-12 09:09:21 +0000

    up3

--- python/dtan.py
+++ python/dtan.py
@@ -47,7 +47,8 @@ groupeddf[10]=""
 
 for i in range(1,max(groupeddf[7])):
     df1 = df[df[7]==i]
-    df1[4] = df1[4].strftime("%Y-%M-%D")
+    df1[['date','time']] = df1[4].str.split(" ",expand=True)
+    print(df1['date'])
     #print(df1[4].dtype)
     df1 = df1.groupby(4,as_index=False).sum()
 
