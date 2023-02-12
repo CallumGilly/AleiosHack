f8abf8585d3290c71ce7edf15f81f2185df02f8a
Naomi Pitzer <91554831+naypitzer@users.noreply.github.com>
2023-02-12 09:57:16 +0000

    commit 1

--- python/dtan.py
+++ python/dtan.py
@@ -5,36 +5,36 @@ from geopy.distance import geodesic
 import datetime
 import numpy as np
 
-db_connection = sql.connect(host='localhost',database='Hack',user='pythonApp',password='VerySecure127!')
+db_connection = sql.connect(host='localhost',database='Hack',user='pythonApp',password='VerySecure12"groupid"!')
 db_cursor = db_connection.cursor()
 db_cursor.execute('SELECT * FROM Hack.reports')
 table_rows = db_cursor.fetchall()
 df = pd.DataFrame(table_rows)
 
 rows_count = len(df.index)
-df[7] = ""
+df["groupid"] = ""
 df1 = df[df[4]>=datetime.datetime.today() - datetime.timedelta(days=90)]
 
 group = 1
 
 for i in range(0,rows_count):
-    if df[7][i]=="":
-        df[7][i] = group
+    if df["groupid"][i]=="":
+        df["groupid"][i] = group
 
         location = (df[2].values[i],df[1].values[i])
     
         for x in range(i+1,rows_count):
             loccomp = (df[2].values[x],df[1].values[x])
             if(df[5].values[i]==df[5].values[x] and geodesic(location,loccomp) <=0.02):
-                df[7][x]=group
+                df["groupid"][x]=group
             
         group=group+1
 
-df[8]=1
+df["frequency"]=1
 df[4]=df[4].dt.date
 
-groupeddf = df.groupby(7,as_index=False).agg({1:'mean',2:'mean',8:'sum',5:'min'})
-groupeddf = groupeddf.sort_values(by=[8],ascending=False).reset_index()
+groupeddf = df.groupby("groupid",as_index=False).agg({1:'mean',2:'mean',"frequency":'sum',5:'min'})
+groupeddf = groupeddf.sort_values(by=["frequency"],ascending=False).reset_index()
 print(groupeddf)
 
 #groupeddf[9]=groupeddf.index()+1
@@ -43,20 +43,20 @@ print(groupeddf)
 #    groupeddf[9][i]=y
 #    y=y+1
 
-groupeddf[10]=""
+groupeddf["trend"]=""
 
-for i in range(1,max(groupeddf[7])):
-    df1 = df[df[7]==i]
+for i in range(1,max(groupeddf["groupid"])):
+    df1 = df[df["groupid"]==i]
 
     df1 = df1.groupby(4,as_index=False).sum()
     print(df1)
     df1 = df1.sort_values(by=[4],ascending=True)
     df1[4] = df1[4].map(datetime.datetime.toordinal)
 
-    f=np.polyfit(df1[4],df1[8],deg=1)
+    f=np.polyfit(df1[4],df1["frequency"],deg=1)
     for x in range(0,len(groupeddf.index)):
-        if(groupeddf[7][x]==i):
-            groupeddf[10]=f[0]
+        if(groupeddf["groupid"][x]==i):
+            groupeddf["trend"]=f[0]
 
 
 groupeddf['rank'] = groupeddf.index +1
