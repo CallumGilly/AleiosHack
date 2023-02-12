62716a247bcdf2d7270e48a2a606b307f0563222
Naomi Pitzer <91554831+naypitzer@users.noreply.github.com>
2023-02-12 11:10:20 +0000

    commit1

--- python/dtan.py
+++ python/dtan.py
@@ -64,7 +64,7 @@ groupeddf.rename(columns={5:'category',1:'longitude',2:'latitude'},inplace=True)
 print(groupeddf)
 
 db_cursor.execute('DELETE FROM Hack.analysis')
-query = "INSERT INTO `Hack`.`analysis` (avglongitude,avglatitude,groupid,frequency,category,rank,trend) VALUES (%s, %s,%s,%s,%s,%s,%s)"
+query = "INSERT INTO `Hack`.`analysis` (avglongitude,avglatitude,groupid,frequency,category,trend,rank) VALUES (%s, %s,%s,%s,%s,%s,%s)"
 
 #groupeddf.to_csv(r'/home/cwjg21/AleiosHack/python/analytics.txt',header=True,index=None,sep=',')
 
@@ -77,6 +77,6 @@ for i in range(0,len(groupeddf.index)):
     rank = int(groupeddf["rank"][i])
     trend = str(groupeddf["trend"][i])
     
-    db_cursor.execute(query,(longitude,latitude,groupid,frequency,category,rank,trend))
+    db_cursor.execute(query,(longitude,latitude,groupid,frequency,category,trend,rank))
 
 #groupeddf.to_csv(r'/home/cwjg21/AleiosHack/python/analytics.txt',header=True,index=None,sep=',')
