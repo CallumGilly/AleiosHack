998c9b23464dfb11f48985a2cf30041f3e4f6717
Naomi Pitzer <91554831+naypitzer@users.noreply.github.com>
2023-02-12 10:02:27 +0000

    commit 1

--- python/dtan.py
+++ python/dtan.py
@@ -60,5 +60,6 @@ for i in range(1,max(groupeddf["groupid"])):
 
 
 groupeddf['rank'] = groupeddf.index +1
+groupeddf.rename(columns={5:'category'})
 print(groupeddf)
 groupeddf.to_csv(r'/home/cwjg21/AleiosHack/python/analytics.txt',header=True,index=None,sep=',')
