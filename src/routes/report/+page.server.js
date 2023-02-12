1934db281831cfd7f57013d0f7659861db5dad75
Callum Gilchrist <43007634+CallumGilly@users.noreply.github.com>
2023-02-12 09:31:21 +0000

    test output

--- src/routes/report/+page.server.js
+++ src/routes/report/+page.server.js
@@ -1,5 +1,7 @@
 import query from "$lib/server/database";
-import fs from "fs";
+const {spawn} = require('child_process');
+
+// import fs from "fs";
 export const actions = {
 	default: async (event) => {
 		const data = await event.request.formData();
@@ -14,19 +16,31 @@ export const actions = {
 
 		// let reader = new FileReader();
 		// reader.readAsDataURL(image);
-		let buffer = Buffer.from(await image.text());
+		// let buffer = Buffer.from(await image.text());
 		try {
 			// reader.onload(() => {
 				query("INSERT INTO Hack.reports (Longitude, Latitude, Image, Time, Category, Description) VALUES (?, ?, ?, NOW(), ?, ?);",
 					[location.long, location.lat, image.name, type, description]
-				);
-				fs.writeFileSync(process.env.HOME + "/AleiosHack/static/images/" + image.name, buffer.toString("base64"), {flag: "w"}, (err) => {
-					if (err) {
-						console.log(err);
-					} else {
-						console.log("File writen... maybe");
-					}
-				});
+				)
+				console.log(image);
+				// .then(() => {
+				// 	try {
+				// 		const python = spawn('python3', [process.env.HOME + "/AleiosHack/python/dtan.py"]);
+				// 		python.on("close", (code) => {
+				// 			console.log(`Python ran with code ${code}`);
+				// 		});
+				// 	} catch (err) {
+				// 			console.log("Python could not run:");
+				// 			console.log(err);
+				// 	}
+				// });
+				// fs.writeFileSync(process.env.HOME + "/AleiosHack/static/images/" + image.name, buffer.toString("base64"), {flag: "w"}, (err) => {
+				// 	if (err) {
+				// 		console.log(err);
+				// 	} else {
+				// 		console.log("File writen... maybe");
+				// 	}
+				// });
 			// })
 			
 
