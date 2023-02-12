df84a3f602d3f8fea75335760b6cefd1be533cdf
Naomi Pitzer <91554831+naypitzer@users.noreply.github.com>
2023-02-12 02:34:34 +0000

    update w cols

--- python/dtan.py
+++ python/dtan.py
@@ -8,26 +8,26 @@ db_cursor = db_connection.cursor()
 db_cursor.execute('SELECT * FROM Hack.reports')
 table_rows = db_cursor.fetchall()
 df = pd.DataFrame(table_rows)
-print(df)
 
 rows_count = len(df.index)
 df["Group"] = ""
+print(df)
 
 group = 1
 
 for i in range(0,rows_count):
-    if df["Group"][i]=="":
-        df["Group"][i] = group
+    if df[7][i]=="":
+        df[7][i] = group
 
-        location = (df["latitude"].values[i],df["longitude"].values[i])
+        location = (df[2].values[i],df[1].values[i])
     
         for x in range(i+1,rows_count):
-            loccomp = (df["latitude"].values[x],df["longitude"].values[x])
-            if(df["prob"].values[i]==df["prob"].values[x] and geodesic(location,loccomp) <=0.02):
-                df["Group"][x]=group
+            loccomp = (df[2].values[x],df[1].values[x])
+            if(df[5].values[i]==df[5].values[x] and geodesic(location,loccomp) <=0.02):
+                df[7][x]=group
             
         group=group+1
 
-df["Frequency"] =1
+df[8] =1
 
-groupeddf = df.groupby("Group",as_index=False).agg({'longitude':'mean','latitude':'mean','Frequency':'sum'})
+groupeddf = df.groupby(7,as_index=False).agg({1:'mean',2:'mean',8:'sum'})
